7b3f6e3495dc4cb5083232b70792b1ec8f9dc3ed
Jose Ruiz <jruiz793ric@gmail.com>
2024-08-22 19:47:58 -0300

    Consumiendo un API

---
+++ app.js
@@ -0,0 +1,24 @@
+async function lufyGears() {
+    try {
+        const res = await fetch("https://api.api-onepiece.com/v2/luffy-gears/en")
+        const data = await res.json();
+
+        data.forEach(gear => {
+            const gearElement = document.createElement(`div`)
+            const nameElement = document.createElement(`h2`)
+            const descriptionElement = document.createElement(`p`)
+
+            nameElement.textContent = `Name ${gear.name}`
+            descriptionElement.textContent = `Description ${gear.description}`
+
+            gearElement.appendChild(nameElement)
+            gearElement.appendChild(descriptionElement)
+
+            document.getElementById(`gears`).appendChild(gearElement)
+        });
+    } catch (error) {
+        console.log("No se puede mostrar la informacion de la API", error)
+    }
+}
+
+lufyGears()
